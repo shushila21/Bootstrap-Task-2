af3bc1eb540cc802d9cd4a588f61aedef3ef15f2
shushila <shushila.budhathoki28@gmail.com>
2022-10-15 23:57:11 +0545

    Task 2 committed sucessfully

---
+++ Task 2 - Bootstrap/icomoon-v1.0/liga.js
@@ -0,0 +1,81 @@
+/* A polyfill for browsers that don't support ligatures. */
+/* The script tag referring to this file must be placed before the ending body tag. */
+
+/* To provide support for elements dynamically added, this script adds
+   method 'icomoonLiga' to the window object. You can pass element references to this method.
+*/
+(function () {
+    'use strict';
+    function supportsProperty(p) {
+        var prefixes = ['Webkit', 'Moz', 'O', 'ms'],
+            i,
+            div = document.createElement('div'),
+            ret = p in div.style;
+        if (!ret) {
+            p = p.charAt(0).toUpperCase() + p.substr(1);
+            for (i = 0; i < prefixes.length; i += 1) {
+                ret = prefixes[i] + p in div.style;
+                if (ret) {
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+    var icons;
+    if (!supportsProperty('fontFeatureSettings')) {
+        icons = {
+            'home': '&#xe900;',
+            'house': '&#xe900;',
+            'location2': '&#xe901;',
+            'map-marker2': '&#xe901;',
+            'bell': '&#xe902;',
+            'alarm2': '&#xe902;',
+            'switch': '&#xe903;',
+            'eye-blocked': '&#xe904;',
+            'views4': '&#xe904;',
+            'google': '&#xe905;',
+            'brand2': '&#xe905;',
+            'feed2': '&#xe906;',
+            'rss': '&#xe906;',
+            'github': '&#xe907;',
+            'brand40': '&#xe907;',
+            'wordpress': '&#xe908;',
+            'brand44': '&#xe908;',
+            'apple': '&#xe909;',
+            'brand53': '&#xe909;',
+          '0': 0
+        };
+        delete icons['0'];
+        window.icomoonLiga = function (els) {
+            var classes,
+                el,
+                i,
+                innerHTML,
+                key;
+            els = els || document.getElementsByTagName('*');
+            if (!els.length) {
+                els = [els];
+            }
+            for (i = 0; ; i += 1) {
+                el = els[i];
+                if (!el) {
+                    break;
+                }
+                classes = el.className;
+                if (/icomoon-liga/.test(classes)) {
+                    innerHTML = el.innerHTML;
+                    if (innerHTML && innerHTML.length > 1) {
+                        for (key in icons) {
+                            if (icons.hasOwnProperty(key)) {
+                                innerHTML = innerHTML.replace(new RegExp(key, 'g'), icons[key]);
+                            }
+                        }
+                        el.innerHTML = innerHTML;
+                    }
+                }
+            }
+        };
+        window.icomoonLiga();
+    }
+}());
